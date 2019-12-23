db4b3f3737b9f58e2aa8323cd626bd67ed02ba13
Alex <dacortejhexson@gmail.com>
2019-12-23 15:18:06 -0500

    Updates

---
+++ precache-manifest.37fa8b2c868c9d77bc41a5bda5f0dce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55fad0c2bc43287cfbbdc48865884179",
+    "url": "/AlexDacorte/ReactMovieDB/index.html"
+  },
+  {
+    "revision": "5a1cbc98ca4a7982cc5b",
+    "url": "/AlexDacorte/ReactMovieDB/static/css/main.dabe5b26.chunk.css"
+  },
+  {
+    "revision": "800891cbe0ca15eb8aa7",
+    "url": "/AlexDacorte/ReactMovieDB/static/js/2.ddc58675.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/AlexDacorte/ReactMovieDB/static/js/2.ddc58675.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a1cbc98ca4a7982cc5b",
+    "url": "/AlexDacorte/ReactMovieDB/static/js/main.da7939c0.chunk.js"
+  },
+  {
+    "revision": "484410b4359ddbe34a60",
+    "url": "/AlexDacorte/ReactMovieDB/static/js/runtime-main.40924c75.js"
+  }
+]);
